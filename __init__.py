32d669381d81f9dd43577dfa589ab3f2b30b07a5
tomchor <tomaschor@gmail.com>
2020-10-29 14:48:32 -0400

    cleaned old core and plotting routines

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ __version__ = '0.1'
 print('Initiating devilpy version %s'%__version__, end='...')
 #from .core import *
 #from .routines import *
-from . import utils, routines, plot, plot2, physics
+from . import utils, routines, plot, physics
 from . import vector, numerical, stats, spectral#, nml
 from . import io, input
 print('done')
--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .simClass import *
-from .outClass import *
-from .dmClass import *
-from .langClass import *
-from .concentration import *
